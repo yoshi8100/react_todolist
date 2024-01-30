bf7579bb64fe7f3e0880c97b0c095b8600b4263f
yassy <yoshida.haya8@gmail.com>
2024-01-30 22:16:03 +0900

    htmlにあたる部分をjsxで作成

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,9 +2,39 @@ import "./styles.css";
 
 export const Todo = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODOです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li>
+            <p>TODOです</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODOでした</p>
+            <button>戻す</button>
+          </li>
+          <li>
+            <p>TODOです</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
