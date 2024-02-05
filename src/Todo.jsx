0d9501d08d231d9dacdb9c57a64c813fd5df9cc4
yassy <yoshida.haya8@gmail.com>
2024-02-06 02:31:26 +0900

    コンポーネント化して整理する

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { InputTodolist } from "./compornents/InputTodolist";
+import { IncompleteTodolist } from "./compornents/IncompleteTodolist";
+import { CompleteTodolist } from "./compornents/CompleteTodolist";
 import "./styles.css";
 
 //　TODO一覧
@@ -48,41 +51,17 @@ export const Todo = () => {
 
   return (
     <>
-      <div className="input-area">
-        <input
-          placeholder="TODOを入力"
-          value={todoText}
-          onChange={onChangeTodoText}
-        />
-        <button onClick={onClickAdd}>追加</button>
-      </div>
-      <div className="incomplete-area">
-        <p className="title">未完了のTODO</p>
-        <ul>
-          {incompleteTodos.map((todo, index) => (
-            <li key={todo}>
-              <div className="list-row">
-                <p className="todo-item">{todo}</p>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="complete-area">
-        <p className="title">完了のTODO</p>
-        <ul>
-          {completeTodos.map((todo, index) => (
-            <li key={todo}>
-              <div className="list-row">
-                <p className="todo-item">{todo}</p>
-                <button onClick={() => onClickBack(index)}>戻す</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <InputTodolist
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
+      <IncompleteTodolist
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodolist todos={completeTodos} onClickBack={onClickBack} />
     </>
   );
 };
---
+++ src/compornents/InputTodolist.jsx
@@ -0,0 +1,10 @@
+export const InputTodolist = (props) => {
+    const { todoText, onChange, onClick } = props;
+    return (
+      <div className="input-area">
+        <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
+        <button onClick={onClick}>追加</button>
+      </div>
+    );
+  };
+  
