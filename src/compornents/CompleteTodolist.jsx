a9d72c563f45756a1974f6f92cae8f3eb5d31db1
yassy <yoshida.haya8@gmail.com>
2024-02-06 21:17:32 +0900

    styleをjsxで管理

--- src/compornents/CompleteTodolist.jsx
+++ src/compornents/CompleteTodolist.jsx
@@ -1,13 +1,38 @@
-export const CompleteTodolist = (props) => {
+const styleCompleteArea = {
+    border: "2px solid #aacfd0",
+    width: "400px",
+    minHeight: "200px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px",
+    backgroundColor: "#c9dede",
+  };
+  
+  const styleTitle = {
+    textAlign: "center",
+    marginTop: 0,
+    fontWeight: "bold",
+  };
+  
+  const styleListRow = {
+    display: "flex",
+    alignItems: "center",
+  };
+  
+  const styleTodoItem = {
+    margin: "6px",
+  };
+  
+  export const CompleteTodolist = (props) => {
     const { todos, onClickBack } = props;
     return (
-      <div className="complete-area">
-        <p className="title">完了のTODO</p>
+      <div style={styleCompleteArea}>
+        <p style={styleTitle}>完了のTODO</p>
         <ul>
           {todos.map((todo, index) => (
             <li key={todo}>
-              <div className="list-row">
-                <p className="todo-item">{todo}</p>
+              <div style={styleListRow}>
+                <p style={styleTodoItem}>{todo}</p>
                 <button onClick={() => onClickBack(index)}>戻す</button>
               </div>
             </li>
--- src/compornents/InputTodolist.jsx
+++ src/compornents/InputTodolist.jsx
@@ -1,7 +1,16 @@
-export const InputTodolist = (props) => {
+const style = {
+    backgroundColor: "#c6e5d9",
+    width: "400px",
+    height: "30px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px",
+  };
+  
+  export const InputTodolist = (props) => {
     const { todoText, onChange, onClick } = props;
     return (
-      <div className="input-area">
+      <div style={style}>
         <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
         <button onClick={onClick}>追加</button>
       </div>
